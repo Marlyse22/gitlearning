6903b05ff47d03329621b2d8f31c71094e0072c3
Ben-Jamin MK <benjaminmk9@gmail.com>
2023-08-23 00:38:51 +0200

    file(file_for_js): adding the explore library

---
+++ watching/file_for_pr.js
@@ -0,0 +1,6 @@
+function explore(element) {
+ return element | "No exploration available.";
+}
+
+// ----------------------------------------------------------------
+// Must complete the exploration
